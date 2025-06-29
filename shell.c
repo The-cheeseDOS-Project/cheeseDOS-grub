45c0789b78579172514e0e7c625e64e755ab76f6
bluMATRIKZ <blumatrikz@gmail.com>
2025-06-29 08:11:22 -0700

    version -> ver

--- shell.c
+++ shell.c
@@ -126,8 +126,8 @@ void shell_execute(const char* cmd) {
     }
 
     if (kstrcmp(command, "help") == 0) {
-        print("Commands: help, clear, print, version, hello, ls, see, add, rem, mkd, cd, calc\n");
-    } else if (kstrcmp(command, "version") == 0) {
+        print("Commands: help, clear, print, ver, hello, ls, see, add, rem, mkd, cd, calc\n");
+    } else if (kstrcmp(command, "ver") == 0) {
         print("cheeseDOS alpha by Connor Thomson\n");
     } else if (kstrcmp(command, "hello") == 0) {
         print("Hello, world!\n");
