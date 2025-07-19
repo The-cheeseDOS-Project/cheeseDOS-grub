376d126d099a8d0e517bffd3fc74e861495a745f
Connor Thomson <blumatrikz@gmail.com>
2025-07-18 20:51:36 -0700

    Make it way more powerful

--- calc.c
+++ calc.c
@@ -15,46 +15,194 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
- 
+
 #include "calc.h"
 #include "string.h"
 #include "vga.h"
+#include <stdint.h>
 
-extern int str_to_int(const char *str);
+#define MAX_DIGITS 128
 
-static void print_int(int value) {
-    char buf[12];
+typedef struct {
+    uint32_t digits[MAX_DIGITS];
+    int size;
+    int sign;
+} big32_t;
+
+static void big32_add(const big32_t* a, const big32_t* b, big32_t* result);
+
+static void big32_zero(big32_t* num) {
+    for (int i = 0; i < MAX_DIGITS; i++) num->digits[i] = 0;
+    num->size = 0;
+    num->sign = 1;
+}
+
+static void big32_copy(const big32_t* src, big32_t* dest) {
+    for (int i = 0; i < MAX_DIGITS; i++) dest->digits[i] = src->digits[i];
+    dest->size = src->size;
+    dest->sign = src->sign;
+}
+
+static int big32_compare(const big32_t* a, const big32_t* b) {
+    if (a->size != b->size) return (a->size > b->size) ? 1 : -1;
+    for (int i = a->size - 1; i >= 0; i--) {
+        if (a->digits[i] != b->digits[i])
+            return (a->digits[i] > b->digits[i]) ? 1 : -1;
+    }
+    return 0;
+}
+
+static void big32_parse(big32_t* num, const char* str) {
+    big32_zero(num);
+    int len = (int)kstrlen(str);
     int i = 0;
-    int is_negative = 0;
+    if (str[0] == '-') {
+        num->sign = -1;
+        i = 1;
+    }
+    for (; i < len; i++) {
+        char c = str[i];
+        if (c >= '0' && c <= '9') {
+            uint32_t carry = c - '0';
+            for (int j = 0; j < num->size || carry; j++) {
+                if (j == num->size) num->digits[num->size++] = 0;
+                uint64_t val = (uint64_t)num->digits[j] * 10 + carry;
+                num->digits[j] = (uint32_t)(val & 0xFFFFFFFF);
+                carry = (uint32_t)(val >> 32);
+            }
+        }
+    }
+}
 
-    if (value == 0) {
+static void big32_print(const big32_t* num) {
+    big32_t tmp;
+    big32_copy(num, &tmp);
+    char buf[MAX_DIGITS * 10];
+    int idx = 0;
+    if (tmp.size == 0 || (tmp.size == 1 && tmp.digits[0] == 0)) {
         putchar('0');
+        print("\n");
         return;
     }
-    if (value < 0) {
-        is_negative = 1;
-        value = -value;
+    while (tmp.size > 0) {
+        uint32_t rem = 0;
+        for (int i = tmp.size - 1; i >= 0; i--) {
+            uint64_t val = ((uint64_t)rem << 32) | tmp.digits[i];
+            tmp.digits[i] = (uint32_t)(val / 10);
+            rem = (uint32_t)(val % 10);
+        }
+        buf[idx++] = '0' + rem;
+        while (tmp.size > 0 && tmp.digits[tmp.size - 1] == 0) tmp.size--;
     }
-
-    while (value != 0 && (size_t)i < sizeof(buf) - 1) {
-        buf[i++] = (value % 10) + '0';
-        value /= 10;
-    }
-    if (is_negative) buf[i++] = '-';
-
-    for (int j = i - 1; j >= 0; j--) putchar(buf[j]);
+    if (num->sign == -1) putchar('-');
+    for (int i = idx - 1; i >= 0; i--) putchar(buf[i]);
+    print("\n");
 }
 
-static int is_operator(char c) {
-    return c == '+' || c == '-' || c == '*' || c == '/';
+static void big32_sub(const big32_t* a, const big32_t* b, big32_t* result) {
+    if (a->sign != b->sign) {
+        big32_t t = *b;
+        t.sign *= -1;
+        big32_add(a, &t, result);
+        return;
+    }
+    if (big32_compare(a, b) < 0) {
+        big32_sub(b, a, result);
+        result->sign = -1;
+        return;
+    }
+    big32_zero(result);
+    result->sign = a->sign;
+    int borrow = 0;
+    for (int i = 0; i < a->size; i++) {
+        int64_t x = a->digits[i];
+        int64_t y = (i < b->size) ? b->digits[i] : 0;
+        int64_t diff = x - y - borrow;
+        if (diff < 0) {
+            diff += (1LL << 32);
+            borrow = 1;
+        } else {
+            borrow = 0;
+        }
+        result->digits[i] = (uint32_t)diff;
+    }
+    result->size = a->size;
+    while (result->size > 0 && result->digits[result->size - 1] == 0) result->size--;
+    if (result->size == 0) result->sign = 1;
 }
 
-void calc_command(const char *expr) {
-    int a = 0, b = 0;
+static void big32_add(const big32_t* a, const big32_t* b, big32_t* result) {
+    if (a->sign == b->sign) {
+        big32_zero(result);
+        result->sign = a->sign;
+        uint64_t carry = 0;
+        int max = (a->size > b->size) ? a->size : b->size;
+        for (int i = 0; i < max || carry; i++) {
+            uint64_t x = (i < a->size) ? a->digits[i] : 0;
+            uint64_t y = (i < b->size) ? b->digits[i] : 0;
+            uint64_t sum = x + y + carry;
+            result->digits[i] = (uint32_t)(sum & 0xFFFFFFFF);
+            carry = sum >> 32;
+        }
+        result->size = max;
+        if (carry && result->size < MAX_DIGITS)
+            result->digits[result->size++] = (uint32_t)carry;
+    } else {
+        big32_t t = *b;
+        t.sign *= -1;
+        big32_sub(a, &t, result);
+    }
+}
+
+static void big32_mul(const big32_t* a, const big32_t* b, big32_t* result) {
+    big32_zero(result);
+    result->sign = a->sign * b->sign;
+    for (int i = 0; i < a->size; i++) {
+        uint64_t carry = 0;
+        for (int j = 0; j < b->size || carry; j++) {
+            uint64_t x = result->digits[i + j];
+            uint64_t y = (j < b->size) ? (uint64_t)a->digits[i] * b->digits[j] : 0;
+            uint64_t sum = x + y + carry;
+            result->digits[i + j] = (uint32_t)(sum & 0xFFFFFFFF);
+            carry = sum >> 32;
+        }
+    }
+    result->size = a->size + b->size;
+    while (result->size > 0 && result->digits[result->size - 1] == 0) result->size--;
+    if (result->size == 0) result->sign = 1;
+}
+
+static void big32_divmod(const big32_t* a, const big32_t* b, big32_t* quotient, big32_t* remainder) {
+    big32_zero(quotient);
+    big32_zero(remainder);
+    big32_t tmp = *a;
+    tmp.sign = 1;
+    big32_t bv = *b;
+    bv.sign = 1;
+    for (int i = tmp.size * 32 - 1; i >= 0; i--) {
+        for (int j = remainder->size; j > 0; j--) {
+            remainder->digits[j] = remainder->digits[j - 1];
+        }
+        remainder->digits[0] = (tmp.digits[i / 32] >> (i % 32)) & 1;
+        remainder->size++;
+        while (remainder->size > 0 && remainder->digits[remainder->size - 1] == 0) remainder->size--;
+        if (big32_compare(remainder, &bv) >= 0) {
+            big32_sub(remainder, &bv, remainder);
+            quotient->digits[i / 32] |= (1U << (i % 32));
+        }
+    }
+    quotient->size = tmp.size;
+    quotient->sign = a->sign * b->sign;
+    while (quotient->size > 0 && quotient->digits[quotient->size - 1] == 0) quotient->size--;
+    remainder->sign = a->sign;
+    if (remainder->size == 0) remainder->sign = 1;
+}
+
+void calc_command(const char* expr) {
+    big32_t a, b, r, mod;
     char op = 0;
     int pos = 0;
     int length = (int)kstrlen(expr);
-
     while (pos < length && expr[pos] == ' ') pos++;
     int start = pos;
     while (pos < length && expr[pos] >= '0' && expr[pos] <= '9') pos++;
@@ -62,40 +210,53 @@ void calc_command(const char *expr) {
         print("Invalid expression\n");
         return;
     }
-    a = str_to_int(expr + start);
-
+    char buf_a[128];
+    int len_a = pos - start;
+    for (int i = 0; i < len_a; i++) buf_a[i] = expr[start + i];
+    buf_a[len_a] = 0;
+    big32_parse(&a, buf_a);
     while (pos < length && expr[pos] == ' ') pos++;
-    if (pos >= length || !is_operator(expr[pos])) {
-        print("Invalid expression\n");
+    if (pos >= length) {
+        print("Invalid operator\n");
         return;
     }
     op = expr[pos++];
     while (pos < length && expr[pos] == ' ') pos++;
-
     start = pos;
     while (pos < length && expr[pos] >= '0' && expr[pos] <= '9') pos++;
     if (pos == start) {
         print("Invalid expression\n");
         return;
     }
-    b = str_to_int(expr + start);
-
-    int result = 0;
-    switch (op) {
-        case '+': result = a + b; break;
-        case '-': result = a - b; break;
-        case '*': result = a * b; break;
-        case '/':
-            if (b == 0) {
-                print("Error: Division by zero\n");
-                return;
-            }
-            result = a / b;
-            break;
-        default:
-            print("Invalid operator\n");
+    char buf_b[128];
+    int len_b = pos - start;
+    for (int i = 0; i < len_b; i++) buf_b[i] = expr[start + i];
+    buf_b[len_b] = 0;
+    big32_parse(&b, buf_b);
+    if (op == '+') {
+        big32_add(&a, &b, &r);
+        big32_print(&r);
+    } else if (op == '-') {
+        big32_sub(&a, &b, &r);
+        big32_print(&r);
+    } else if (op == '*') {
+        big32_mul(&a, &b, &r);
+        big32_print(&r);
+    } else if (op == '/') {
+        if (b.size == 0) {
+            print("Error: Division by zero\n");
             return;
+        }
+        big32_divmod(&a, &b, &r, &mod);
+        big32_print(&r);
+    } else if (op == '%') {
+        if (b.size == 0) {
+            print("Error: Division by zero\n");
+            return;
+        }
+        big32_divmod(&a, &b, &r, &mod);
+        big32_print(&mod);
+    } else {
+        print("Unknown operator\n");
     }
-    print_int(result);
-    print("\n");
 }
