c1990436410e2380ff78bb1ab79065ddc5a9e9d3
Connor Thomson <blumatrikz@gmail.com>
2025-07-18 20:35:43 -0700

    Update rtc.h to work with new rtc.c

--- rtc.h
+++ rtc.h
@@ -22,12 +22,12 @@
 #include <stdint.h>
 
 typedef struct {
-    uint8_t second;
-    uint8_t minute;
-    uint8_t hour;
-    uint8_t day;
-    uint8_t month;
     uint16_t year;
+    uint8_t  month;
+    uint8_t  day;
+    uint8_t  hour;
+    uint8_t  minute;
+    uint8_t  second;
 } rtc_time_t;
 
 void read_rtc_time(rtc_time_t* time);
