e3c3d3d06b0f03dbc7fd37c3d32a9ff6cc75dfa4
Connor Thomson <blumatrikz@gmail.com>
2025-07-18 20:22:23 -0700

    Change color palate

--- vga.h
+++ vga.h
@@ -22,13 +22,13 @@
 #include <stdint.h>
 
 #define COLOR_BLACK   0x0
-#define COLOR_BLUE    0x1
-#define COLOR_GREEN   0x2
-#define COLOR_CYAN    0x3
-#define COLOR_RED     0x4
-#define COLOR_MAGENTA 0x5
-#define COLOR_YELLOW  0x6
-#define COLOR_WHITE   0x7
+#define COLOR_BLUE    0x9
+#define COLOR_GREEN   0xA
+#define COLOR_CYAN    0xB
+#define COLOR_RED     0xC
+#define COLOR_MAGENTA 0xD
+#define COLOR_YELLOW  0xE
+#define COLOR_WHITE   0xF
 
 #define VGA_COLOR(fg, bg) ((bg << 4) | (fg & 0x0F))
 
