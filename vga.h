868e5db6f28d2725d02d8a273f0a9fbf29a232ec
Connor Thomson <blumatrikz@gmail.com>
2025-07-18 19:13:42 -0700

    Add color

--- vga.h
+++ vga.h
@@ -19,13 +19,26 @@
 #ifndef VGA_H
 #define VGA_H
 
+#include <stdint.h>
+
+#define COLOR_BLACK   0x0
+#define COLOR_BLUE    0x1
+#define COLOR_GREEN   0x2
+#define COLOR_CYAN    0x3
+#define COLOR_RED     0x4
+#define COLOR_MAGENTA 0x5
+#define COLOR_YELLOW  0x6
+#define COLOR_WHITE   0x7
+
+#define VGA_COLOR(fg, bg) ((bg << 4) | (fg & 0x0F))
+
 void putchar(char c);
 void print(const char* str);
 void clear_screen();
 void backspace();
-
 void set_cursor(int position);
 void set_cursor_pos(int pos);
 int get_cursor();
+void set_text_color(uint8_t fg, uint8_t bg);
 
 #endif
