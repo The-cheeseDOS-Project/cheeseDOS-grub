f9c4a606f4f7f1e07f9d7985d956b9f67b23fb67
Connor Thomson <blumatrikz@gmail.com>
2025-07-31 13:19:15 -0700

    Time offset to 0

--- src/rtc/rtc.c
+++ src/rtc/rtc.c
@@ -33,7 +33,7 @@
 #define RTC_STATUS_B      0x0B
 #define RTC_CENTURY       0x32
 
-#define TIMEZONE_OFFSET   17
+#define TIMEZONE_OFFSET   0
 
 static uint8_t get_rtc_register(uint8_t reg) {
     outb(CMOS_ADDRESS, reg);
